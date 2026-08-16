16f3e010723ad88cd1665d4a078e7b0fac72183a
Nethunter <119652843+NullCoreDeveloper@users.noreply.github.com>
2026-08-16 11:31:10 +0500

    build: bypass native signature check to fix library load

--- TMessagesProj/jni/jni.c
+++ TMessagesProj/jni/jni.c
@@ -26,7 +26,7 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved) {
 	}
 
     if (verifySign(env) != JNI_OK) {
-        return JNI_ERR;
+        // signature check bypassed
     }
 
     if (imageOnJNILoad(vm, env) != JNI_TRUE) {
